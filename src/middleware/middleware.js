8e184c0b8424df1e3d2ee850f99707bc5723ab16
shivam21bb106 <shivam21bbbb@gmail.com>
2025-06-14 11:10:13 +0530

    implement jwt

---
+++ src/middleware/middleware.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+
+export function middleware(request){
+    const token=request.cookies.get('token')?.value;
+
+    if(!token){
+        return NextResponse.redirect(new URL('/login',request.url))
+    }
+    try{
+        jwt.verify(token, process.env.JWT_SECRET);
+        return NextResponse.next();
+    }
+    catch(err){
+        console.error(err);
+        return NextResponse.redirect(new URL('/login',request.url));
+    }
+}
