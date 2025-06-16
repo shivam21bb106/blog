2c4b0c29eb4dde4a2fd8a5bf6c3f33e95f50bfd3
shivam21bb106 <shivam21bbbb@gmail.com>
2025-06-16 14:26:52 +0530

    post models schema

--- src/models/Posts.jsx
+++ src/models/Posts.jsx
@@ -1,18 +1,19 @@
 import mongoose from "mongoose";
 
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  tags: {
+    type: String,
+    required: true,
+  },
+});
 
-const postSchema=new mongoose.Schema({
-    title:{
-        type:String,
-        required:true,
-    },
-    content:{
-        type:String,
-        required:true,
-    },
-    tags:{
-        type:String,
-        required:True
-    }
-
-})
+const Posts = mongoose.models.Posts || mongoose.model("Posts", postSchema);
+export default Posts;
